f6776a26912bba017bdcdbede3aa42e9b03eea66
Vankka <20226336+Vankka@users.noreply.github.com>
2020-12-24 17:01:18 +0200

    Bump DiscordSRV version to 1.21.1 and add AccountLinkManager test

--- src/main/java/com/discordsrv/apitest/Plugin.java
+++ src/main/java/com/discordsrv/apitest/Plugin.java
@@ -1,15 +1,40 @@
 package com.discordsrv.apitest;
 
 import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
+import github.scarsz.discordsrv.util.DiscordUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Plugin extends JavaPlugin {
+public class Plugin extends JavaPlugin implements Listener {
 
     private DiscordSRVListener discordsrvListener = new DiscordSRVListener(this);
 
     @Override
     public void onEnable() {
         DiscordSRV.api.subscribe(discordsrvListener);
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String discordId = DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(event.getPlayer().getUniqueId());
+        if (discordId == null) {
+            event.getPlayer().sendMessage(ChatColor.RED + "You are not linked");
+            return;
+        }
+
+        User user = DiscordUtil.getJda().getUserById(discordId);
+        if (user == null) {
+            event.getPlayer().sendMessage(ChatColor.YELLOW + "Couldn't find the user you're linked to");
+            return;
+        }
+
+        event.getPlayer().sendMessage(ChatColor.GREEN + "You're linked to " + user.getAsTag());
     }
 
     @Override
